47616c70f8a67831a10b2799384ebaf1fa590ae2
ilayda06 <canilayda766@gmail.com>
2022-10-27 14:26:46 +0300

    Uygulama1-Nesne Tabanlı Programlama

---
+++ uygulama1/uygulama1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(48 / 6 / 4);
+            Console.WriteLine(24 / 3 * 2);
+            Console.WriteLine((7 + 4) * 2 - 1 + 8 / 2);
+        }
+    }
+}
